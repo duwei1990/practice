a6274f6f9c8cc5efd2f74705aee3ee9062d8c909
duwei <784886313@qq.com>
2014-02-10 11:06:48 +0800

    duwei

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print "how old are you?"
+old=raw_input()
+print "how height are you?"
+height=raw_input()
+print "how much do you heavy?"
+heavy=raw_input()
+
+print "So,you're %r old,%r height and %r heavy"%(old,height,heavy)
---
+++ ex12.py
@@ -0,0 +1,6 @@
+
+age=raw_input("how old are you?")
+height=raw_input("how tall are you?")
+heavy=raw_input("how much do you heavy?")
+
+print "So,you are %r old,%r tall and %r heavy"%(age,height,heavy)
