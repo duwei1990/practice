cc63f49d50c7fa8dd70a82fb7d26e723dbc2530a
duwei <784886313@qq.com>
2014-02-12 21:50:52 +0800

    duwei

--- login.py
+++ login.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from time import sleep
  
 def login(self):
-dr =self.driver
-login_url='http://localhost/wordpress/wp-login.php'
-dr.get(login_url)
-dr.find_element_by_name('log').send_keys('admin')
-dr.find_element_by_name('pwd').send_keys('admin')
-dr.find_element_by_name('wp_submit').click()
+   dr =self.driver
+   login_url='http://localhost/wordpress/wp-login.php'
+   dr.get(login_url)
+   dr.find_element_by_name('log').send_keys('admin')
+   dr.find_element_by_name('pwd').send_keys('admin')
+   dr.find_element_by_name('wp_submit').click()
 sleep(3)
