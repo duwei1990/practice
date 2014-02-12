dd72685b46d558046e3270231c3eca1ca18d35f4
duwei <784886313@qq.com>
2014-02-12 21:02:46 +0800

    duwei

--- edit_post.py
+++ edit_post.py
@@ -4,27 +4,27 @@ from time import sleep
 import login
  
 class WordPressTestCase(unittest.TestCase):
-dr = None
-post_edit_url='http://localhost/wordpress/wp-admin/post.php?post=4&action=edit'
+   dr = None
+   post_edit_url='http://localhost/wordpress/wp-admin/post.php?post=4&action=edit'
  
 def setUp(self):
-self.dr=webdriver.Firefox()
+   self.dr=webdriver.Firefox()
  
 def test_edit_post(self):
-self.login()
-self.edit_post(self)
+   self.login()
+   self.edit_post(self)
  
 def edit_post(self):
-self.dr.get('post_edit_url')
-content='my name is duwei'
-js="document.getElementById('content_ifr').contentWindow.document.body.innerHTML='"+content+"'"
-print js
-self.dr.execute_script(js)
-self.dr.find_element_by_name('publish').click()
-return content
+   self.dr.get('post_edit_url')
+   content='my name is duwei'
+   js="document.getElementById('content_ifr').contentWindow.document.body.innerHTML='"+content+"'"
+   print js
+   self.dr.execute_script(js)
+   self.dr.find_element_by_name('publish').click()
+   return content
  
 def tearDown(self):
-self.dr.quit()
+   self.dr.quit()
  
 if __name__=='__main__':
-unittest.main()
+   unittest.main()
